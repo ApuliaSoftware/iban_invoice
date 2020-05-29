409ceecdf6161de29c0f260137338e46c56502e6
Ilaria Franchini <i.franchini@apuliasoftware.it>
2020-05-29 12:58:29 +0200

    [FIX] Check type invoice

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Iban invoice",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Account",
     "website": "http://www.apuliasoftware.it",
     "author": "Ilaria Franchini",
